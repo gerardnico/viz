b204de9c0468be939aaaf1179c864d609bf23fa0
Gerardnico <gerardnico@gerardnico.com>
2017-09-28 23:11:48 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+(function name() {
+    var section = d3.select("body").append("section");
+    section.append("div")
+        .html("First!");
+    section.append("div")
+        .html("Second!");
+})();
